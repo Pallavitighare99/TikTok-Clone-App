aed9c1d5368be4df0f2ff9f157378e441c84eee1
Pallavitighare99 <pallavitighare01@gmail.com>
2022-10-11 16:28:22 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images : {
+    domains: ['media-exp1.licdn.com', 'lh3.googleusercontent.com']
+
+  }
 }
 
 module.exports = nextConfig
